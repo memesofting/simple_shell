1cf15d31f82821cb69083e4db2cd7ca503ac3b43
memesofting <bibitayoadekunle00@gmail.com>
2023-12-06 14:05:41 +0100

    fork-wait-execve

---
+++ shell_sample/fork_wait_execve.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - fork, wiat, and execve exercise
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+}
--- shell_sample/printarg.c
+++ shell_sample/printarg.c
@@ -5,13 +5,14 @@
  * Return: Always 0
  */
 
-/*int main(int ac __attribute__((unused)), char **av)*/
-int main(int ac, char **av)
+int main(int ac __attribute__((unused)), char **av)
+/*int main(int ac, char **av)*/
 {
 	int i;
 
 	i = 0;
-	while (i < ac)
+	/*while (i < ac)*/
+	while (av[i] != NULL)
 	{
 		printf("%s\n", av[i]);
 		i++;
